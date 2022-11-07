612a8cb2fd36bbece04666f5cbd024d5e3bf90af
carlk <carloskachuk@gmail.com>
2022-11-07 09:25:32 -0300

    feat: the alert service has been created

---
+++ src/services/alert/Alert.js
@@ -0,0 +1,12 @@
+import Swal from 'sweetalert2';
+export const alert = {
+    confirmation: (type, title, text) => {
+    
+    } ,
+    error: (type, title, text) =>{
+
+    } ,
+    information: (title, text) =>{
+
+    } 
+}
