e20019246e2799b27cd1dc9c715c7f98f6197634
carlk <carloskachuk@gmail.com>
2022-11-07 16:13:30 -0300

    feat: the confirmation method has been implemented

--- src/services/alert/Alert.js
+++ src/services/alert/Alert.js
@@ -1,12 +1,36 @@
-import Swal from 'sweetalert2';
+import Swal from 'sweetalert2'
+
 export const alert = {
-    confirmation: (type, title, text) => {
-    
-    } ,
-    error: (type, title, text) =>{
-
-    } ,
-    information: (title, text) =>{
-
-    } 
+  confirmation: (toast, title, text) => {
+    if (toast === true) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      return Toast.fire({
+        icon: 'success',
+        title,
+        text
+      })
+    } else {
+      Swal.fire({
+        icon: 'success',
+        title,
+        text
+      })
+    }
+  },
+  error: (type, title, text) => {
+    return 1
+  },
+  information: (title, text) => {
+    return 1
+  }
 }
