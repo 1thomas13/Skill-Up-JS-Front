a26b4fb2ce7d89f29f73a7ddc594ed58dfa58732
Juan Sevillano <sevillanojuanmanuel@gmail.com>
2022-11-08 14:13:29 -0300

    Custom hook for API usage and Loader Component

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,37 +1,29 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-const { URL } = process.env
-
 const token = localStorage.getItem('token')
 
 const instance = axios.create({
-  baseURL: URL,
+  baseURL: import.meta.env.VITE_URL,
   headers: token && { authorization: `Bearer ${token}` }
 })
 
-const getData = (endpoint) => {
+const useApi = ({ endpoint, method }) => {
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState(null)
-
   useEffect(() => {
-    instance
-      .get(endpoint)
+    instance[method](endpoint)
       .then((res) => setData(res.data))
       .catch((err) => setError(err))
       .finally(() => setIsLoading(false))
-  }, [endpoint])
+  }, [endpoint, method])
+
   return { data, isLoading, error }
 }
 
-export const postData = async (endpoint) => {
-  const res = await instance.post(endpoint)
-  console.log(res)
-}
-
-const useApi = () => {
-  return { getData, postData }
-}
+// USE
+// Ex. = const { data, isLoading, error } = useApi({ endpoint: 'users', method: 'get' })
+// console.log(data)
 
 export default useApi
