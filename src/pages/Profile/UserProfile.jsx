1c39045dcbee630c98ac28afa5684ff237c80bfe
carlk <carloskachuk@gmail.com>
2022-11-09 08:17:58 -0300

    feat: user profile skeleton was implemented

--- src/pages/Profile/UserProfile.jsx
+++ src/pages/Profile/UserProfile.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import Box from '@mui/material/Box'
+import { Card, CardActions, CardContent, Typography } from '@mui/material'
+import { CustomButton } from '../../Components/CustomButton'
+
+const styleCard = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  pt: 2,
+  px: 4,
+  pb: 3,
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column'
+}
 
 export const UserProfile = () => {
+  const fields = [{ name: 'Nombre' }, { name: 'Apellido' }, { name: 'Email' }, { name: 'Rol' }]
   return (
-    <Box>
-        <h1>User profile</h1>
-    </Box>
+    <Card sx={{ ...styleCard }}>
+      <CardContent sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+        <Typography variant='h4' sx={{ mb: 5, mt: 2 }}>User profile</Typography>
+        {fields.map((field) => {
+          return <Typography key={field.name} sx={{ m: 4 }} variant='h6'>{field.name}: </Typography>
+        })}
+      </CardContent>
+      <CardActions sx={{ m: 6 }}>
+        <CustomButton>Editar usuario</CustomButton>
+        <CustomButton>Cambiar contrasena</CustomButton>
+        <CustomButton>Borrar usuario</CustomButton>
+      </CardActions>
+    </Card>
   )
 }
