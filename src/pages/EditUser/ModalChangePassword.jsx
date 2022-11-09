2687976e7d51ec33c7c4163c63966c4416c218cb
carlk <carloskachuk@gmail.com>
2022-11-09 09:39:24 -0300

    feat: the modal change password has been created and implemented

---
+++ src/pages/EditUser/ModalChangePassword.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Box, Modal } from '@mui/material'
+import { CustomButton } from '../../Components/CustomButton'
+import { ChangePassword } from './ChangePassword'
+
+export const ModalChangePassword = () => {
+  const [openChangePassword, setOpenChangePassword] = React.useState(false)
+  const handleOpenChangePassword = () => {
+    setOpenChangePassword(true)
+  }
+  const handleCloseChangePassword = () => {
+    setOpenChangePassword(false)
+  }
+  return (
+    <Box>
+        <CustomButton onClick={handleOpenChangePassword}>Cambiar contrasena</CustomButton>
+        <Modal
+        open={openChangePassword}
+        onClose={handleCloseChangePassword}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+        >
+            <ChangePassword/>
+        </Modal>
+    </Box>
+  )
+}
