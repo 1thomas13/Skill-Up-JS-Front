6fa480fe0c6127389491dc373de038293566a051
carlk <carloskachuk@gmail.com>
2022-11-10 07:46:35 -0300

    feat: get user state and display have been implemented

--- src/pages/Profile/UserProfile.jsx
+++ src/pages/Profile/UserProfile.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Card, CardActions, CardContent, Typography } from '@mui/material'
 import { CustomButton } from '../../Components/CustomButton'
 import { ModalChangePassword } from '../EditUser/ModalChangePassword'
+import { useSelector } from 'react-redux'
+import { Loader } from '../../Components/Loader'
 
 const styleCard = {
   position: 'absolute',
@@ -18,13 +20,20 @@ const styleCard = {
 }
 
 export const UserProfile = () => {
-  const fields = [{ name: 'Nombre' }, { name: 'Apellido' }, { name: 'Email' }, { name: 'Rol' }]
+  const userData = useSelector((state) => state.user)
+
+  const fields = [
+    { name: 'Nombre', value: userData.body !== undefined ? userData.body.firstName : (<Loader/>) },
+    { name: 'Apellido', value: userData.body !== undefined ? userData.body.lastName : (<Loader/>) },
+    { name: 'Email', value: userData.body !== undefined ? userData.body.email : (<Loader/>) },
+    { name: 'Rol', value: userData.body !== undefined ? userData.body.rol : (<Loader/>) }
+  ]
   return (
     <Card sx={{ ...styleCard }}>
       <CardContent sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
         <Typography variant='h4' sx={{ mb: 5, mt: 2 }}>User profile</Typography>
         {fields.map((field) => (
-          <Typography key={field.name} sx={{ m: 4 }} variant='h6'>{field.name}: </Typography>
+          <Typography key={field.name} sx={{ m: 4 }} variant='h6'>{field.name}: {field.value}</Typography>
         ))}
       </CardContent>
       <CardActions sx={{ m: 6 }}>
