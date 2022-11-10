3b8449f1ef3dc178834491cfd2789f8fa269da40
carlk <carloskachuk@gmail.com>
2022-11-10 07:42:17 -0300

    feat: the dispatch for get user has been implemented

--- src/pages/Profile/ModalUserProfile.jsx
+++ src/pages/Profile/ModalUserProfile.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Box, Modal, Button } from '@mui/material'
 import { UserProfile } from './UserProfile'
 import Icon from '@mui/material/Icon'
+import { useDispatch } from 'react-redux'
+import { getUser } from '../../app/actions/index'
 
 export const ModalUserProfile = () => {
+  const dispatch = useDispatch()
+  const dispatchUser = useCallback(
+    () => dispatch(getUser()),
+    [dispatch]
+  )
+
   const [open, setOpen] = React.useState(false)
   const handleOpenUserProfile = () => {
     setOpen(true)
+    dispatchUser()
   }
   const handleCloseUserProfile = () => {
     setOpen(false)
   }
+
   return (
     <Box>
         <Button onClick={handleOpenUserProfile}><Icon>person</Icon> User profile</Button>
