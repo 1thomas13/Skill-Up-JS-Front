442b8a63185217ad40c3e52e790dc5d95ee13320
carlk <carloskachuk@gmail.com>
2022-11-11 14:48:57 -0300

    feat: update password and delete user actionas were added

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,5 +1,6 @@
+import { instance } from '../../../utils/instance'
 import axios from 'axios'
-import { GET_CATEGORIES, LOG_USER, UNLOG_USER, GET_USERS } from './types'
+import { GET_CATEGORIES, LOGIN_USER, LOGOUT_USER, GET_USERS } from './types'
 
 const { URL } = import.meta.env.VITE_URL
 
@@ -20,35 +21,27 @@ export function postCategory (payload) {
       const response = await axios.post(`${URL}/categories`, payload)
       return response
     } catch (e) {
-      console.log(e.message)
+
     }
   }
 }
 
 export function createUser (payload) {
   return async function () {
-    try {
-      const response = await axios.post(`${URL}/users`, payload)
-      return response
-    } catch (e) {
-      console.log(e.message)
-    }
+    const response = await axios.post(`${URL}/users`, payload)
+    return response
   }
 }
 
 export function logUser (payload) {
   return async function (dispatch) {
-    try {
-      const response = await axios.post(/* Endpoint de logeo, */ payload)
-      return dispatch({ type: LOG_USER, payload: response.data.user })
-    } catch (e) {
-      console.log(e.message)
-    }
+    const response = await axios.post(/* Endpoint de logeo, */ payload)
+    return dispatch({ type: LOGIN_USER, payload: response.data.user })
   }
 }
 
-export function unlogUser () {
-  return { type: UNLOG_USER }
+export function logout () {
+  return { type: LOGOUT_USER }
 }
 
 export function getUsers () {
@@ -71,4 +64,14 @@ export function createTransaction (payload) {
       console.log(e.message)
     }
   }
+}
+
+export const updatePassword = async (userid, payload) => {
+  const res = await instance.put(`/users/changepassword/${userid}`, payload)
+  return res
+}
+
+export const deleteUser = async (userid) => {
+  const res = await instance.delete(`users/${userid}`)
+  return res
 }
