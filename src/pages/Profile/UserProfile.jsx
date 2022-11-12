8c0ee4735f7f0b5f0068d716107feed4f1a28656
carlk <carloskachuk@gmail.com>
2022-11-12 06:51:24 -0300

    style: the style of user profile was changed

--- src/pages/Profile/UserProfile.jsx
+++ src/pages/Profile/UserProfile.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Card, CardActions, CardContent, Typography } from '@mui/material'
+import { Card, CardActions, CardContent, Typography, Avatar, Box } from '@mui/material'
 import { CustomButton } from '../../Components/CustomButton'
 import { ModalChangePassword } from '../EditUser/ModalChangePassword'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { Loader } from '../../Components/Loader'
+import { alert } from '../../services/alert/Alert'
+import { deleteUser, logout } from '../../app/actions/index'
 
 const styleCard = {
   position: 'absolute',
+  height: '95%',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
@@ -20,26 +23,42 @@ const styleCard = {
 }
 
 export const UserProfile = () => {
-  const userData = useSelector((state) => state.user)
-
+  const userStoreData = useSelector((state) => state.user)
+  const dispatch = useDispatch()
   const fields = [
-    { name: 'Nombre', value: userData.body !== undefined ? userData.body.firstName : (<Loader/>) },
-    { name: 'Apellido', value: userData.body !== undefined ? userData.body.lastName : (<Loader/>) },
-    { name: 'Email', value: userData.body !== undefined ? userData.body.email : (<Loader/>) },
-    { name: 'Rol', value: userData.body !== undefined ? userData.body.rol : (<Loader/>) }
+    { name: 'Nombre', value: userStoreData.user !== undefined ? userStoreData.user.firstName : (<Loader size={35}/>) },
+    { name: 'Apellido', value: userStoreData.user !== undefined ? userStoreData.user.lastName : (<Loader size={35}/>) },
+    { name: 'Email', value: userStoreData.user !== undefined ? userStoreData.user.email : (<Loader size={35}/>) },
+    { name: 'Rol', value: userStoreData.user !== undefined ? userStoreData.user.rol : (<Loader size={35}/>) }
   ]
+
+  const handleDeleteUser = () => {
+    alert.question('Estas seguro?', 'Estas seguro que quieres eliminar el usuario?', 'Si').then((result) => {
+      if (result.isConfirmed) {
+        dispatch(deleteUser(userStoreData.user.id)).then((result) => {
+          if (result.message === 'The user has been deleted') {
+            alert.confirmation(true, 'El usuario ha sido eliminado')
+            dispatch(logout())
+          }
+        })
+      }
+    })
+  }
   return (
     <Card sx={{ ...styleCard }}>
       <CardContent sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-        <Typography variant='h4' sx={{ mb: 5, mt: 2 }}>User profile</Typography>
+        <Typography variant='h4' sx={{ mb: 8 }}>User profile</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Avatar src={userStoreData.user.avatar} sx={{ width: 150, height: 150, mb: 5 }}/>
+        </Box>
         {fields.map((field) => (
-          <Typography key={field.name} sx={{ m: 4 }} variant='h6'>{field.name}: {field.value}</Typography>
+          <Typography key={field.name} sx={{ m: 4, display: 'flex', alignItems: 'center' }} variant='h6'>{field.name}: {field.value}</Typography>
         ))}
       </CardContent>
-      <CardActions sx={{ m: 6 }}>
+      <CardActions sx={{ mt: 8, ml: 5, mr: 5 }}>
         <CustomButton sx={{ mr: 2, ml: 2, width: 200 }}>Editar usuario</CustomButton>
-        <ModalChangePassword/>
-        <CustomButton sx={{ ml: 2, mr: 2, width: 200 }}>Borrar usuario</CustomButton>
+        <ModalChangePassword userId={userStoreData.user.id}/>
+        <CustomButton sx={{ ml: 2, mr: 2, width: 200 }} onClick={handleDeleteUser}>Borrar usuario</CustomButton>
       </CardActions>
     </Card>
   )
